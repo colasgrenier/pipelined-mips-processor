2a92d27b04905e68d72e42c2b8d8349065dda922
colasgrenier <colasgrenier@hotmail.com>
2021-03-29 20:56:17 -0400

    skeleton of memory stage

--- memory.vhd
+++ memory.vhd
@@ -1,12 +1,31 @@
 ENTITY memory IS
 	PORT (
 		clock		: in std_logic;
+		read		: in std_logic;				-- must read from data memory.
+		write		: in std_logic;				-- must write to data memory.
 		address		: in std_logic_vector(31 downto 0);
 		write_data	: in std_logic_vector(31 downto 0);
-		read_data	: out std_logic_vector(31 downto 0);
-		target		: out std_logic_vector(31 downto 0);	-- target register for hazard detection / forwarding.
+		result		: out std_logic_vector(31 downto 0);
+		target		: out std_logic_vector(4 downto 0);	-- target register for hazard detection / forwarding.
 	);
 END ENTITY;
+	
+ARCHITECTURE memory_arch OF memory IS	
+BEGIN
+	PROCESS (clock) BEGIN
+		IF (rising_edge(clock)) THEN
+			IF (memory_read = '1') THEN
+				-- read from main memory then
+				-- result <= memory value.
+			ELSE IF (memory_write = '1') THEN
+				-- write the vlaue to main memory.
+			ELSE
+				-- no read and no write, then the result is the read_data_2
+				output <= read_data_2;
+			END IF;
+		END IF;
+	END PROCESS;
+END memory_arch;
 
 ENTITY data_memory IS
 	PORT (
