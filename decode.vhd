8cf73a7d4aa1c438944a01ca9b7857abb61f7160
colasgrenier <colasgrenier@hotmail.com>
2021-04-13 09:57:31 -0400

    improvements, way simpler now, still need to work on stalling

--- decode.vhd
+++ decode.vhd
@@ -13,22 +13,13 @@ ENTITY decode IS
 		memory_target			: in std_logic_vector(4 downto 0);
 	);
 END decode;
-
-ENTITY register_file IS
-	PORT (
-		clock		: in std_logic;
-		read_address_1	: in std_logic_vector(4 downto 0);
-		read_address_2	: in std_logic_vector(4 downto 0);
-		write_address	: in std_logic_Vector(4 downto 0);
-		write_data	: in std_logic_vector(31 downto 0);
-		read_data_1	: out std_logic_vector(31 downto 0);
-		read_data_2	: out std_logic_vector(31 downto 0);
-	);
-END register_file;
 	
-ARCHITECTURE register_file_arch of register_file IS
+ARCHITECTURE decode_arch of decode IS
 	TYPE register_file_content_type IS ARRAY(31 downto 0) OF std_logic_vector(31 downto 0);
-	SIGNAL register_file_contents	: register_file_content_type;
+	SIGNAL register_file	: register_file_content_type;
+	VARIABLE stall_count	: integer;
+	SIGNAL read_address_1	: std_logic_vector(4 downto 0);
+	SIGNAL read_address_2	: std_logic_vector(4 downto 0);
 BEGIN
 	PROCESS (clock) BEGIN
 		-- Initialize the registers to 0.
@@ -37,105 +28,42 @@ BEGIN
 				register_file_contents(i) <= x"00000000";
 			END LOOP;
 		END IF;
-		-- We write all data on the rising edge.
+		-- We write to the register file on the rising edge.
 		IF rising_edge(clock) THEN
 			IF write_address /= "00000" THEN
 				register_file_contents(to_integer(unsigned(write_address))) <= write_data;
 			END IF;
 		END IF;
-		-- We read all data on the falling edge.
+		-- We read all registers & send the instruction details to execute on the falling edge.
 		IF falling_edge(clock) THEN
-			read_data_1 <= register_file_contents(to_integer(unsigned(read_address_1)));
-			read_data_2 <= register_file_contents(to_integer(unsigned(read_address_2)));
+			CASE instruction(31 downto 26) IS
+				WHEN "0000" =>
+					-- R type instruction.
+					read_data_1 <= register_file_contents(to_integer(unsigned(instruction(25 downto 21))));
+					read_data_2 <= register_file_contents(to_integer(unsigned(instruction(20 downto 16))));
+					target <= instruction(15 downto 11);
+					opcode <= instruction(31 downto 26);
+					shamt <= instruction(10 downto 6);
+					funct <= instruction(5 downto 0);
+				WHEN x"4" | x"5" | x"8" | "" =>
+					-- I type instruction with sign-extension.
+					opcode <= instruction(31 downto 26);
+					read_data_1 <= register_file_contents(to_integer(unsigned(instruction(25 downto 21))));
+					target <= instruction(20 downto 16);
+					immediate <= std_logic_vector(resize(signed(instruction(15 downto 0)), 32));
+				WHEN x"a" | x"c" | x"d" | "001110" =>
+					-- I type instruction with zero-extension.
+					opcode <= instruction(31 downto 26);
+					read_data_1 <= register_file_contents(to_integer(unsigned(instruction(25 downto 21))));
+					target <= instruction(20 downto 16);
+					immediate <= x"0000" & instruction(15 downto 0);
+				WHEN x"2" | x"3" =>
+					-- J type instruction.
+					opcode <= instruction(31 downto 6);
+					immediate <= std_logic_vector(resize(unsigned(I(25 downto 0)), 32));
+			END CASE;
+
 		END IF;
 	END PROCESS;
-	PROCESS (read_address_1, read_address_2) BEGIN
-		
-	END PROCESS;
-END register_file_arch;
-	
-	
-ARCHITECTURE decArch OF decode IS 
---31 Registers of 32 bits each
-TYPE REGISTER_MEM IS ARRAY(31 DOWNTO 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
-
-SIGNAL rFile : REGISTER_MEM;
-
-BEGIN
-	process (clk)
-	begin
-		if rising_edge(clk) then
-			if writeAddr /= "0000" then
-				rFile(to_integer(unsigned(writeAddr))) <= writeData;
-			end if;
-		else if falling_edge(clk) then
-			case unsigned(I(31 downto 26)) is
-				--Register operation
-				when x"00" =>
-				
-				
-				--Jump
-				when x"02" =>
-				
-				--Jump&Link
-				when x"03" =>
-				
-				--BEQ
-				when x"04" =>
-					--Sign extend immediate (?)
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-				
-				--BNE
-				when x"05" =>
-					--Sign extend immediate (?)
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-				
-				--Add Imm
-				when x"08" =>
-					--Register Rs
-					read_data_1 <= rFile(to_integer(unsigned(I(25 downto 21)));
-					--Sign extended Imm val
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-					execOp <= ;
-				
-				--Set Less Than Imm
-				when x"0A" =>
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-				
-				--AND Imm
-				when x"0C" =>
-					--Zero extend Imm
-					immediate <= std_logic_vector(x"00") & I(15 downto 0);
-					
-				
-				--OR Imm
-				when x"0D" =>
-					--Zero extend Imm
-					immediate <= std_logic_vector(x"00") & I(15 downto 0);
-				
-				--XOR Imm
-				when x"0E" =>
-					--Zero extend Imm
-					immediate <= std_logic_vector(x"00") & I(15 downto 0);
-				
-				--Load Upper Imm
-				when x"0F" =>
-					immediate <=  I(15 downto 0) & std_logic_vector(x"00");
-				
-				--Load Word
-				when x"23" =>
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-				
-				--Store Word
-				when x"2B" =>
-					immediate <= std_logic_vector(resize(signed(I(15 downto 0)), 32));
-					
-				when others =>
-				
-			end case;
-		
-		end if;
-	end process;
-END decArch;
-					
+END decode_arch;
 
