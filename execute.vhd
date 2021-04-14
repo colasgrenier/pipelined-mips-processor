c76712fbae9ebf136930000a74dc10607070311f
Yaters <yajallouli@gmail.com>
2021-04-14 17:19:12 -0400

    Name changes

--- execute.vhd
+++ execute.vhd
@@ -13,20 +13,17 @@ ENTITY execute IS
 		immediate			: in std_logic_vector(31 downto 0);   -- immediate value from instruction.
 		
 		
-		execute_use_1_execute 		: in std_logic; --control use execute result in port 1
-		execute_use_2_execute		: in std_logic; --control use execute result in port 2
-		execute_use_1_memory			: in std_logic; --control use mem result in port 1
-		execute_use_2_memory			: in std_logic; --control use mem result in port 2
-		memory_use_memory_in			: in std_logic; --buffer if mem should use mem result
+		execute_1_use_execute 		: in std_logic; --control use execute result in port 1
+		execute_2_use_execute		: in std_logic; --control use execute result in port 2
+		execute_1_use_memory			: in std_logic; --control use mem result in port 1
+		execute_2_use_memory			: in std_logic; --control use mem result in port 2
 		memory_result					: in std_logic_vector(31 downto 0); --result of mem stage (for forwarding)
 		
 		next_target			: in std_logic_vector(4 downto 0);    -- the target register of the next instruction.
 		program_counter	: in std_logic_vector(31 downto 0);   -- next instruction address.
 		
-		memory_use_memory_out		: out std_logic;
-		
 		result				: out std_logic_vector(31 downto 0);  -- result of the ALU operation. (Inout for forwarding)
-		PC_out				: out std_logic_vector(31 downto 0);  -- computed PC address.
+		branch_address		: out std_logic_vector(31 downto 0);  -- computed PC address.
 		branch_taken		: out std_logic;					  		  -- indicated whether the branch has been taken.
 		target				: out std_logic_vector(4 downto 0)    -- the target register of the current phase
     );
@@ -39,9 +36,7 @@ ARCHITECTURE alu_arch OF execute IS
 	 --replaces opcode, Helpful in the switch-case for 8 bits
 	 signal op 	 : std_logic_vector(7 downto 0);
 	 
-	 --Used to ignore last 2 (?) I if a branch was taken
-	 --todo later (flushing on branch)
-	 --signal lastIBranched : std_logic(1 downto 0);
+	 --TODO: use branch_taken to flush first input after branch
 	 
 	 --Operator 1/2, hold ALU inputs (may be forwarded)
 	 signal operator1: std_logic_vector(31 downto 0);
@@ -58,12 +53,12 @@ BEGIN
 	op <= "00" & opcode;
 	
 	--Implement forwarding
-	operator1 <= inner_result when execute_use_1_execute = '1' else
-				memory_result when execute_use_1_memory = '1' else
-				read_data_1;
-	operator2 <= inner_result when execute_use_2_execute = '1' else
-				memory_result when execute_use_2_memory	 = '1' else
-				read_data_2;
+	operator1 <= inner_result when execute_1_use_execute = '1' else
+				    memory_result when execute_1_use_memory = '1' else
+				    read_data_1;
+	operator2 <= inner_result when execute_2_use_execute = '1' else
+				    memory_result when execute_2_use_memory	 = '1' else
+				    read_data_2;
 	
 	PROCESS(clock)
 	BEGIN
@@ -71,11 +66,10 @@ BEGIN
 		IF rising_edge(clock) THEN
 			-- all signals are set to 0.
 			inner_result <= x"00000000";
-			PC_out <= x"00000000";
+			branch_address <= x"00000000";
 			branch_taken <= '0';
 			-- transfer the target register
 			target <= next_target;
-			memory_use_memory_out	 <= memory_use_memory_in;
 			
 			
 			-- operate depending on opcode.
@@ -93,7 +87,7 @@ BEGIN
 							inner_result <= std_logic_vector(shift_right(signed(operator2), to_integer(unsigned(shamt))));
 						WHEN x"08" =>
 							-- jr pc=rs instruction.
-							PC_out <= operator1;
+							branch_address <= operator1;
 							branch_taken <= '1';
 						WHEN x"10" =>
 							-- mfhi rd=hi instruction.
@@ -140,7 +134,7 @@ BEGIN
 				WHEN x"02" =>
 					-- j instruction.
 					--PC = JumpAddress = { PC+4[31:28], address, 2’b0 }
-					PC_out <= std_logic_vector(
+					branch_address <= std_logic_vector(
 									 "+"(unsigned(program_counter), "0100")(31 downto 28))
 								  & immediate(25 downto 0) 
 								  & "00";
@@ -149,7 +143,7 @@ BEGIN
 					-- jal instruction.
 					--PC = JumpAddress = { PC+4[31:28], address, 2’b0 }
 					--R[31] = PC+8
-					PC_out <= std_logic_vector(
+					branch_address <= std_logic_vector(
 									 "+"(unsigned(program_counter), "0100")(31 downto 28))
 								  & immediate(25 downto 0) 
 								  & "00";
@@ -159,7 +153,7 @@ BEGIN
 					-- beq if(rs==rt): pc=pc+4+branchaddr instruction.
 					-- BranchAddr = { 14{immediate[15]}, immediate, 2’b0 }
 					IF operator1 = operator2 THEN
-						PC_out <= std_logic_vector( unsigned(program_counter) + "0100" + 
+						branch_address <= std_logic_vector( unsigned(program_counter) + "0100" + 
 															  unsigned((immediate(29 downto 0)&"00")));
 						branch_taken <= '1';
 					END IF;
@@ -167,7 +161,7 @@ BEGIN
 					-- bne if(rs!=rt): pc=pc+4+branchaddr instruction.
 					-- BranchAddr = { 14{immediate[15]}, immediate, 2’b0 }
 					IF operator1 /= operator2 THEN
-						PC_out <= std_logic_vector( unsigned(program_counter) + "0100" + 
+						branch_address <= std_logic_vector( unsigned(program_counter) + "0100" + 
 															  unsigned((immediate(29 downto 0)&"00")));
 						branch_taken <= '1';
 					END IF;				
