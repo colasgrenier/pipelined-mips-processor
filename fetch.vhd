19925ec2fe9d86d547bacb714ca8876883415cfb
colasgrenier <colasgrenier@hotmail.com>
2021-04-14 09:43:03 -0400

    fetch now works

--- fetch.vhd
+++ fetch.vhd
@@ -9,7 +9,7 @@ use ieee.numeric_std.all;
 ENTITY fetch IS
 	PORT (
 		clock		: in std_logic;				-- clock signal.
-		c_wait		: in std_logic;				-- whether to continue to the next address or not.
+		stall		: in std_logic;				-- whether to continue to the next address or not.
 		branch_taken	: in std_logic;				-- determines whether to branch for the computed address.
 		branch_address	: in std_logic_vector(31 downto 0);	-- computed address for branch.
 		instruction	: out std_logic_vector(31 downto 0)	-- instruction that was read.
@@ -17,9 +17,9 @@ ENTITY fetch IS
 END ENTITY;
 
 ARCHITECTURE fetch_arch OF fetch IS
-	TYPE instruction_memory_type		IS ARRAY (8095 downto 0) OF std_logic_vector(31 downto 0);
+	TYPE instruction_memory_type		IS ARRAY (8195 downto 0) OF std_logic_vector(31 downto 0);
 	SIGNAL instruction_memory_contents	: instruction_memory_type;
-	SIGNAL program_counter			: std_logic_vector(31 downto 0);
+	SIGNAL program_counter			: std_logic_vector(31 downto 0) := x"00000000";
 	
 BEGIN
 process(clock)
@@ -30,33 +30,38 @@ process(clock)
 	
 	begin
 	  
-	IF (now < 1ps) THEN
-		file_open(instruction_memory_file, "instruction_memory.dat", READ_MODE);
-		FOR line IN 0 TO 8095 LOOP
+	IF (now < 1 ps) THEN
+		REPORT "initializing instruction memory...";
+		instruction <= x"00000000";
+		file_open(instruction_memory_file, "program.txt", READ_MODE);
+		FOR line IN 0 TO 8195 LOOP
 			readline(instruction_memory_file, instruction_memory_line);
 			read(instruction_memory_line, instruction_memory_data);
+			-- report " inst addr cont = " & integer'image(to_integer(unsigned(instruction_memory_data)));
 			instruction_memory_contents(line) <= instruction_memory_data;
 		END LOOP;
 		file_close(instruction_memory_file);
+		REPORT "instruction memory initialization complete";
 	END IF;
 	-- Execute the request.
 	IF rising_edge(clock) THEN
-		-- Put the requested content.
-		instruction <= instruction_memory_contents(to_integer(unsigned(program_counter)));
 		-- Check the signals
-		IF c_wait = '1' THEN
+		IF stall = '1' THEN
 			-- do nothing, as the decode stage has asked to wait.
 		ELSE
 			IF branch_taken = '1' THEN
 				-- the ALU has computed that the branch must be taken.
-				program_counter <= branch_address;
+				program_counter <= branch_address + 4;
+				-- Put the instruction at the next program counter.
+				instruction <= instruction_memory_contents(to_integer(unsigned(branch_address(31 downto 2))));
 			ELSE
-				-- no branching, increment the program counter by 4.
+				-- Put the current instruction.
+				instruction <= instruction_memory_contents(to_integer(unsigned(program_counter(31 downto 2))));
+				-- No branching, increment the program counter by 4.
 				program_counter <= program_counter + 4;
 			END IF;
 		END IF;
+		
 	END IF;
 end process;
 END fetch_arch;
-
-
