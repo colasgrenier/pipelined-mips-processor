2b84540b35d5d82fe1c07742aca812aecb66ae6d
colasgrenier <colasgrenier@hotmail.com>
2021-04-18 15:54:01 -0400

    created writeback.vhd, we need it after all!

---
+++ writeback.vhd
@@ -0,0 +1,13 @@
+ENTITY writeback IS
+	PORT (
+		clock		: in std_logic;
+		input		: in std_logic_vector(31 downto 0);
+		result		: out std_logic_vector(31 downto 0);
+	);
+END ENTITY;
+	
+ARCHITECTURE writeback_arch OF writeback IS BEGIN
+	PROCESS (clock) BEGIN
+		result <= input;
+	END PROCESS;
+END writeback_arch;
