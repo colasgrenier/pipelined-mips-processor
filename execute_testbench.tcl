58a46d652c8c55277488199c3782fe406ba86984
colasgrenier <colasgrenier@hotmail.com>
2021-04-20 18:22:13 -0400

    added some more signals

--- execute_testbench.tcl
+++ execute_testbench.tcl
@@ -1,33 +1,45 @@
 proc AddWaves {} {
-	add wave -position end sim:/execute_testbench/clock
-	add wave -position end sim:/execute_testbench/etb_opcode
-	add wave -position end sim:/execute_testbench/etb_shamt
-	add wave -position end sim:/execute_testbench/etb_funct
-	add wave -position end sim:/execute_testbench/etb_rd_1
-	add wave -position end sim:/execute_testbench/etb_rd_2
-	add wave -position end sim:/execute_testbench/etb_immediate
-    add wave -position end sim:/execute_testbench/etb_target
-    add wave -position end sim:/execute_testbench/etb_pc
-    add wave -position end sim:/execute_testbench/etb_result
-    add wave -position end sim:/execute_testbench/etb_branch_address
-    add wave -position end sim:/execute_testbench/etb_execute_1
-    add wave -position end sim:/execute_testbench/etb_execute_2
-    add wave -position end sim:/execute_testbench/etb_memory_1
-    add wave -position end sim:/execute_testbench/etb_memory_2
-    add wave -position end sim:/execute_testbench/etb_memory_result
-    add wave -position end sim:/execute_testbench/etb_branch_taken
-    add wave -position end sim:/execute_testbench/etb_next_target
+	add wave -position end sim:/processor_testbench/clock
+	add wave -position end -hex sim:/processor_testbench/proc/decode_stage/instruction
+	add wave -position end sim:/processor_testbench/proc/decode_stage/stall_fetch
+	add wave -position end -dec sim:/processor_testbench/proc/decode_stage/execute_target
+	add wave -position end -dec sim:/processor_testbench/proc/decode_stage/memory_target
+	add wave -position end -dec sim:/processor_testbench/proc/decode_stage/writeback_target
+	add wave -position end -hex sim:/processor_testbench/proc/decode_stage/opcode
+	add wave -position end -hex sim:/processor_testbench/proc/decode_stage/funct
+	add wave -position end sim:/processor_testbench/proc/execute_stage/execute_1_use_execute
+	add wave -position end sim:/processor_testbench/proc/execute_stage/execute_2_use_execute
+	add wave -position end sim:/processor_testbench/proc/execute_stage/execute_1_use_memory
+	add wave -position end sim:/processor_testbench/proc/execute_stage/execute_2_use_memory
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/read_data_1
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/read_data_2
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/immediate
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/result
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/branch_address
+	add wave -position end -dec sim:/processor_testbench/proc/execute_stage/branch_taken
+	add wave -position end sim:/processor_testbench/proc/memory_stage/memory_read
+	add wave -position end sim:/processor_testbench/proc/memory_stage/memory_write
+	add wave -position end sim:/processor_testbench/proc/memory_stage/memory_use_memory
+	add wave -position end -dec sim:/processor_testbench/proc/memory_stage/address
+	add wave -position end -dec sim:/processor_testbench/proc/memory_stage/write_data
+	add wave -position end -dec sim:/processor_testbench/proc/memory_stage/result
 }
 
 vlib work
 
+vcom fetch.vhd
+vcom decode.vhd
 vcom execute.vhd
-vcom execute_testbench.vhd
+vcom memory.vhd
+vcom processor.vhd
+vcom processor_testbench.vht
 
-vsim execute_testbench
+vsim processor_testbench
 
 force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1ns
 
 AddWaves
 
 run 100ns
+
+wave zoom full
