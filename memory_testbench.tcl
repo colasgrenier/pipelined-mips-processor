d7c07dddd795f42efcf425a00edebacd3602a437
colasgrenier <colasgrenier@hotmail.com>
2021-04-21 21:05:18 -0400

    Update memory_testbench.tcl

--- memory_testbench.tcl
+++ memory_testbench.tcl
@@ -1,10 +1,10 @@
 proc AddWaves {} {
 	add wave -position end sim:/memory_testbench/clock
-	add wave -position end sim:/memory_testbench/mtb_address
-	add wave -position end sim:/memory_testbench/mtb_data
+	add wave -position end -hex sim:/memory_testbench/mtb_address
+	add wave -position end -dec sim:/memory_testbench/mtb_data
 	add wave -position end sim:/memory_testbench/mtb_read
 	add wave -position end sim:/memory_testbench/mtb_write
-	add wave -position end sim:/memory_testbench/mtb_result
+	add wave -position end -dec sim:/memory_testbench/mtb_result
 }
 
 vlib work
@@ -18,4 +18,4 @@ force -deposit clock 0 0 ns, 1 0.5 ns -repeat 1ns
 
 AddWaves
 
-run 100ns
+run 10ns
